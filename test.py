0d7df45bbca4e98df4f5f350fc686a531a0ae107
d-bev <dmbevi29@gmail.com>
2023-03-26 14:51:42 -0700

    init

---
+++ test.py
@@ -0,0 +1,14 @@
+import time
+
+import RPi.GPIO as GPIO
+
+CHANNEL = 19
+
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(CHANNEL, GPIO.OUT)
+    GPIO.output(CHANNEL, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(CHANNEL, GPIO.LOW)
+finally:
+    GPIO.cleanup()
